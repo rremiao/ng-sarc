e76ce868e46352f33dd0274a97024cebd9af4373
Raphael <remiao.raphael.dev@gmail.com>
2023-05-04 20:12:31 -0300

    database config

--- src/main/java/com/rremiao/ngsarc/domain/adapter/CaracteristicsAdapter.java
+++ src/main/java/com/rremiao/ngsarc/domain/adapter/CaracteristicsAdapter.java
@@ -2,9 +2,11 @@ package com.rremiao.ngsarc.domain.adapter;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 import com.rremiao.ngsarc.domain.entity.Caracteristics;
 
-@Component
-public interface CaracteristicsAdapter  extends JpaRepository<Caracteristics, Integer>{
+@Repository
+public interface CaracteristicsAdapter extends JpaRepository<Caracteristics, Integer>{
     
 }
